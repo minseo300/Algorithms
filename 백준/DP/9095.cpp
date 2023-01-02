1579ed10fc478f07367029515dc36fc8d7289c7b
root <lmslmsms0616@gmail.com>
2023-01-02 17:49:13 +0900

    1.2 DP

---
+++ 백준/DP/9095.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+
+    int dp[11] = { 0 };
+    //초깃값
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+ 
+    //각 숫자마다 방법의 수 구하기
+    for (int i = 4; i < 11; i++)
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+ 
+    int T, n;
+    cin>>T;
+ 
+    //결과 출력
+    for (int test_case = 0; test_case < T; test_case++) {
+        cin>>n;
+        cout<<dp[n]<<"\n";
+    }
+ 
+    return 0;
+}
