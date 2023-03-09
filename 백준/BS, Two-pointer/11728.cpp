582e9c4edfe038e67c628edc16c5a48576240fc3
root <lmslmsms0616@gmail.com>
2023-03-09 17:36:05 +0900

    0309 TP

---
+++ 백준/BS, Two-pointer/11728.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <unordered_map>
+#include <stack>
+#include<cstring>
+using namespace std;
+
+int N, M;
+
+void TwoPointer(vector<int> &A, vector<int> &B)
+{
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    int ptrA = 0;
+    int ptrB = 0;
+
+    vector<int> answer(N + M);
+    int idx = 0;
+    while (ptrA < N && ptrB < M)
+    {
+        if (A[ptrA] < B[ptrB])
+        {
+            answer[idx++] = A[ptrA];
+            ptrA++;
+        }
+        else
+        {
+            answer[idx++] = B[ptrB];
+            ptrB++;
+        }
+    }
+
+    while (ptrA < N)
+        answer[idx++] = A[ptrA++];
+    while (ptrB < M)
+        answer[idx++] = B[ptrB++];
+
+    for (int i : answer)
+        cout << i << ' ';
+}
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+
+    vector<int> A(N);
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+
+    vector<int> B(M);
+    for (int i = 0; i < M; i++)
+        cin >> B[i];
+
+    TwoPointer(A, B);
+}
